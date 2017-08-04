690b4c1a3213e105906b2990b2dc6b7e02aa9cff
KatsuyaAkasaka <akasakatora1208@gmail.com>
2017-08-04 14:27:19 +0900

    アカウント作成完了 ログイン不可能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(email: params[:session][:email].downcase)
-        if user && user.authenticate(params[:session][:password])
-            log_in user
-            redirect_to user
-        else
-            flash.now[:danger] = 'Invalid email/password combination'
-            render 'new'
+        render 'new'
     end
 
     def destroy
