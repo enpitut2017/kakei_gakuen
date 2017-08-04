97a43c58f424e4def7cb77b4b7f4bc1cf7604f34
kobya4 <sp.16.sy+git@gmail.com>
2017-08-04 16:44:43 +0900

    Update config/enviroment

---
+++ public/assets/script-1988572c6965879b7a35ea446f315faf64d636470bd0379bc5d0dd6a0e095015.js
@@ -0,0 +1,38 @@
+function parse_input(){
+  str = $("#result_text").val();
+  if(str){
+    str = str.replace(/\s|　|\.|\,|円/g, '');
+    item = str.replace(/\d+/g, ' ');
+    cost = str.replace(/\D+/g, ' ');
+    items = item.split(' ');
+    costs = cost.split(' ');
+    items = $.grep(items, function(e){return e !== "";});
+    costs = $.grep(costs, function(e){return e !== "";});
+    console.log(items, costs);
+    for (i=0; i<items.length; i++) {
+      console.log(i);
+      $('#add_items').append('<input type="text" name="items[]" value="' + items[i] + '">');
+      $('#add_items').append('<input type="text" name="costs[]" value="' + costs[i] + '"><br>');
+    }
+    $('#submit').show();
+  } else {
+    $('#result_text').val('文字列を入力してください。');
+  }
+}
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+var recognition = new webkitSpeechRecognition();
+recognition.lang = 'ja';
+
+// 録音終了時トリガー
+recognition.addEventListener('result', function(event){
+    var text = event.results.item(0).item(0).transcript;
+    $("#result_text").val(text);
+}, false);
+
+// 録音開始
+function record()
+{
+    recognition.start();
+}
+;
