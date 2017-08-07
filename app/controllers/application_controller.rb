5a25958db64e60caca6c04758df2e406d44e4bdd
Tomoya Mikami <trymorecc@gmail.com>
2017-08-07 11:07:33 +0900

    経験値の計算

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include SessionsHelper
+
+    def test(items)
+        defalt_exp = 10
+        times = Array.new
+        d = Time.parse(Date.today.strftime("%Y-%m-%d")).to_i
+        items.each do |item|
+            times.push(((d-Time.parse(item).to_i)/100)/864)
+        end
+        times.each do |time|
+            defalt_exp -= time
+        end
+        if defalt_exp < 1 do
+            defalt_exp = 1
+        end
+    end
 end
