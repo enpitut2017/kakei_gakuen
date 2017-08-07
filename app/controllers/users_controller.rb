dbaf4acfe04499cbf815f8ce53711fbfdbce3e6f
Tomoya Mikami <trymorecc@gmail.com>
2017-08-07 15:35:01 +0900

    経験値とレベルの動作チェック完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,19 +14,13 @@ class UsersController < ApplicationController
   def show
     @lost = 0
     @rest = 0
+    @img_path = image_path(@user.level)
     books = Book.where(user: @user)
     books.each do |book|
       @lost += book.cost
     end
     @rest = @user.budget - @lost
     @books = @user.books.order("id DESC").limit(5)
-
-    @exp = @user.exp
-    @level = level_culcurate
-
-    @user.level = @level
-    @user.save
-
   end
 
   # GET /users/new
@@ -112,22 +106,5 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
-    end
-        #レベル計算
-    def level_culcurate
-        exp = current_user.exp
-        if exp == nil then
-            exp = 0
-        end
-        #debugger
-
-        level = 0
-        levelup_table = [0, 10, 20]
-        size = levelup_table.size
-        tmpexp = exp
-        while (level < size) and ((tmpexp -= levelup_table[level]) >= 0) do
-            level += 1
-        end
-        return level
     end
 end
