62971547ac448da2ead16e97f62b1f3e3878b8c6
MinamiKaori <m.kaori25@gmail.com>
2017-08-07 13:08:03 +0900

    top index edit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( script.js )
+Rails.application.config.assets.precompile += %w( custom.css )
 
