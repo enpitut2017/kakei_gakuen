d5f725bff5676e7a78fdc5c3400c502d32ad67ad
nanaho <nyanto1124@gmail.com>
2017-08-07 10:53:00 +0900

    フレンドリーフォワーディングを実装したがなんか動いてないような

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,4 +18,15 @@ module SessionsHelper
     def logged_in?
         !current_user.nil?
     end
+    
+# 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
