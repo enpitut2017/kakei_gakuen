e05faeaf36758fcf41d23d6b0f9bb388ac961102
Tomoya Mikami <trymorecc@gmail.com>
2017-10-25 00:17:39 +0900

    ミスの修正

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ export RAILS_ENV=staging
 bundle exec pumactl -S /home/yatteiki/rails/kakei_gakuen/shared/tmp/pids/puma.state stop
 git pull origin develop
 bundle install --without test development
-bundle exec rails db:migrate RAILS_ENV=production
-bundle exec rails assets:precompile RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=staging
+bundle exec rails assets:precompile RAILS_ENV=staging
 export SECRET_KEY_BASE=bundle exec rake secret
 export RAILS_ENV=staging
-bundle exec puma -t 5:5 -e production -C config/puma.rb -d
+bundle exec puma -t 5:5 -e staging -C config/puma.rb -d
