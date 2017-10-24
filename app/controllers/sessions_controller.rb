1296c0168a3650c028b7b0ae11bee75265136f60
赤坂勝哉 <akasakatora1208@gmail.com>
2017-10-24 10:26:26 +0900

    out: 経験値、レベル in: コイン edit画面を2種類用意

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
-        log_in user
-        redirect_back_or user
+            log_in user
+            redirect_back_or user
         else
             flash.now[:danger] = 'Invalid email/password combination'
             render 'new'
