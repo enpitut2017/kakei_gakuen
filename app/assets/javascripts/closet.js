9d2b1c0b22673cc2e021420d2d8b963e9812ced4
kobya4 <sp.16.sy+git@gmail.com>
2017-11-10 17:09:20 +0900

    closets proto improve

--- app/assets/javascripts/closet.js
+++ app/assets/javascripts/closet.js
@@ -2,13 +2,8 @@ function init() {
 
 }
 
-function draw_image(path) {
-  canvas = document.getElementById('ClosetsCharactor');
-  context = canvas.getContext('2d');
-
-  image = new Image();
-  image.src = path;
-  image.addEventListener('load', function() {
-      context.drawImage(image, 0, -10, canvas.width, canvas.height);
-  }, false);
+function draw_image(id, path) {
+  id = '#' + id;
+  console.log(path);
+  $(id).css('background-image', 'url('+ path +')');
 }
