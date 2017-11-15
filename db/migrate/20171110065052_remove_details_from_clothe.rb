b7703c71ec6e51f9e9a47e70513546e5ec02b0fd
Tomoya_Mikami <trymorecc@gmail.com>
2017-11-15 12:42:40 +0900

    いらないマイグレーションの削除

--- db/migrate/20171110065052_remove_details_from_clothe.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDetailsFromClothe < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :clothes, :created_at, :timestamp
-    remove_column :clothes, :updated_at, :timestamp
-  end
-end
