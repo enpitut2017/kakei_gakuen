5867f93eb7b95b3d64d872879758c278ca378c38
kobya4 <sp.16.sy+git@gmail.com>
2017-11-15 14:01:10 +0900

    tmp

--- app/assets/javascripts/closet.js
+++ app/assets/javascripts/closet.js
@@ -5,7 +5,7 @@ function switch_items(category) {
 function draw_image(id, path, zindex) {
   id = '#' + id;
   $(id).css('background-image', 'url('+ path +')');
-  //$(id).css('zIndex', zindex);
+  $(id).css('zIndex', zindex);
 }
 
 function build_json() {
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113053402) do
+ActiveRecord::Schema.define(version: 20171115064310) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171113053402) do
     t.string "name"
     t.integer "price"
     t.string "image"
+    t.integer "priority"
   end
 
   create_table "clothes_tags_links", force: :cascade do |t|
@@ -75,7 +76,6 @@ ActiveRecord::Schema.define(version: 20171113053402) do
     t.string "tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "priority"
   end
 
   create_table "user_has_clothes", force: :cascade do |t|
