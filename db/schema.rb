79feeed8cf1330d7ca04b9b30f77e3c83a52f281
kobya4 <sp.16.sy+git@gmail.com>
2017-11-17 15:34:32 +0900

    tmp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115012946) do
+ActiveRecord::Schema.define(version: 20171115064310) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -69,6 +69,13 @@ ActiveRecord::Schema.define(version: 20171115012946) do
   end
 
   create_table "managers", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "manages", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
     t.datetime "created_at", null: false
