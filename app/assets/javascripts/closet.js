3a9750d46b7aa535ac68cdbcdf70a2b68c315de5
kobya4 <sp.16.sy+git@gmail.com>
2017-11-22 15:17:29 +0900

    Add: Switch Category Function

--- app/assets/javascripts/closet.js
+++ app/assets/javascripts/closet.js
@@ -1,5 +1,16 @@
 function switch_items(category) {
+  id = '#' + category + '-group';
 
+  $('#ClosetsItems').children().remove();
+  items = $(id).data('hoge');
+  console.log(items);
+  $.each(items, function(i, item){
+    console.log(item);
+    $('#ClosetsItems').append('<div class="col-md-6 col-sm-12">' +
+                              '<div class="ItemBox" onclick="draw_image(' + item.category + ', ' + item.path + ', ' + item.priority + ', ' + item.id + ')">' +
+                              '<img src=' + item.path +'>' +
+                              '</div></div>');
+  });
 }
 
 function draw_image(id, path, zindex, cloth_id) {
@@ -16,8 +27,6 @@ function build_json() {
   $('.Layers').each(function(i, elm) {
     users_wear[$(elm).attr('id')] = $(elm).children().val();
   });
-
-  console.log(users_wear);
 
   $.post('/closets', users_wear ,function(data){
     console.log(data);
