b5d8dffc7feb58cd22310d84875d001999a75249
KatsuyaAkasaka <akasakatora1208@gmail.com>
2017-11-22 12:39:55 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -77,13 +77,6 @@ ActiveRecord::Schema.define(version: 20171115012946) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "manages", force: :cascade do |t|
-    t.string "name"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string "tag"
   end
