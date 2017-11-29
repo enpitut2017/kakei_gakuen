2244cf0242dc2d87400f5f8197733d81d3c8c482
Tomoya Mikami <trymorecc@gmail.com>
2017-11-29 11:30:56 +0900

    ログイン、新規作成、画像の取得

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115012946) do
+ActiveRecord::Schema.define(version: 20171128102616) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20171115012946) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
   end
 
 end
