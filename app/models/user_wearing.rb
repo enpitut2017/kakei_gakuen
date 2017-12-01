2260c60d0c28c0a0a6170e7abed1325fafb68297
Tomoya Mikami <trymorecc@gmail.com>
2017-12-01 17:01:44 +0900

    userwearingの修正

--- app/models/user_wearing.rb
+++ app/models/user_wearing.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class UserWearing < ApplicationRecord
 
     def self.get_user_wearing_array(user_id)
@@ -15,14 +14,3 @@ class UserWearing < ApplicationRecord
 end
 
 
-=======
-class UserWearing < ApplicationRecord
-
-    def self.get_user_wearing_array (user_id)
-        user_wearing = UserWearing.find_by(user_id: user_id)
-        return [user_wearing.upper_clothes, user_wearing.lower_clothes, user_wearing.sox, user_wearing.front_hair, user_wearing.back_hair, user_wearing.face] 
-    end
-end
-
-
->>>>>>> origin/develop
