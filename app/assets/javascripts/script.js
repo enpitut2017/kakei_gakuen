b87338b7e7054fe40e5dcb10f354f9b067a5def7
Tomoya Mikami <trymorecc@gmail.com>
2017-12-17 23:15:19 +0900

    Revert "Merge pull request #277 from enpitut2017/feature/add-snapshot-function"
    This reverts commit 7e2fa863180d007a4f290600426ae456d1b52c42, reversing
    changes made to f4242cc5061cd15a30f4909ee3aee27acfd1b490.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -184,11 +184,3 @@ function escape_html(string) {
     }[match]
   });
 }
-
-function snapshot() {
-  html2canvas(document.querySelector("#Charactor-Img")).then(canvas => {
-    base64 = canvas.toDataURL('image/png');
-    //console.log(base64);
-    return base64
-  });
-}
