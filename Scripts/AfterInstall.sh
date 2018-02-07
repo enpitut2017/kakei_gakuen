58b837b3e4dc4031564bbfe5faca1c09bc777e47
谷口正樹 <hassaku@Hassaku-mbp.local>
2018-02-07 16:15:52 +0900

    edit Afterinstall.sh robots.txt

--- Scripts/AfterInstall.sh
+++ Scripts/AfterInstall.sh
@@ -1,1 +1,1 @@
-su -l yatteiki_deploy -c 'sudo chown -R yatteiki_deploy /var/www/yatteiki && cd /var/www/yatteiki && cp -r /home/yatteiki_deploy/backup/public ./ && bundle install --without test development &&  bundle exec rails db:migrate RAILS_ENV=production && bundle exec rails assets:precompile RAILS_ENV=production'
+su -l yatteiki_deploy -c 'sudo chown -R yatteiki_deploy /var/www/yatteiki && cd /var/www/yatteiki && bundle install --without test development &&  bundle exec rails db:migrate RAILS_ENV=production && bundle exec rails assets:precompile RAILS_ENV=production'
